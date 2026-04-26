69be776570f4b6420da6d50aecc0a574543a1b9c
Ren Usuda <rururu.developer@gmail.com>
2026-04-27 02:28:22 +0900

    refactor: router.dartをrouting/配下に移動する

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pub/src/router.dart';
+import 'package:pub/src/routing/router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
