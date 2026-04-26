42c8b2aec8facde51a042dbf0ef9b2515b6d44ea
Ren Usuda <rururu.developer@gmail.com>
2026-04-26 16:12:35 +0900

    refactor: DioのbaseUrl設定を利用

--- lib/data/remote/api_packages_remote_data_source.dart
+++ lib/data/remote/api_packages_remote_data_source.dart
@@ -2,17 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:pub/domain/package.dart';
 import 'package:pub/data/remote/packages_remote_data_source.dart';
 import 'package:pub/data/remote/packages_response_dto.dart';
-import 'package:pub/data/remote/pub_api.dart';
 
 class ApiPackagesRemoteDataSource implements PackagesRemoteDataSource {
-  ApiPackagesRemoteDataSource({Dio? dio}) : _dio = dio ?? Dio();
+  ApiPackagesRemoteDataSource({Dio? dio})
+    : _dio = dio ?? Dio(BaseOptions(baseUrl: 'https://pub.dev/api/'));
 
   final Dio _dio;
 
   @override
   Future<List<Package>> fetchPackages() async {
-    final uri = PubApi.endpoint('packages');
-    final response = await _dio.getUri<Map<String, Object?>>(uri);
+    final response = await _dio.get<Map<String, Object?>>('packages');
     final dto = PackagesResponseDto.fromJson(response.data!);
     return dto.packages.map((package) => package.toDomain()).toList();
   }
