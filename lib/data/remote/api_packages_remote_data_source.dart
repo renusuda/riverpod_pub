d0b43dd30014548a28be36208df37e1d9fb4691b
Ren Usuda <rururu.developer@gmail.com>
2026-04-26 15:57:28 +0900

    refactor: パッケージ取得をDioに置き換え

--- lib/data/remote/api_packages_remote_data_source.dart
+++ lib/data/remote/api_packages_remote_data_source.dart
@@ -1,27 +1,19 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:pub/domain/package.dart';
 import 'package:pub/data/remote/packages_remote_data_source.dart';
 import 'package:pub/data/remote/packages_response_dto.dart';
 import 'package:pub/data/remote/pub_api.dart';
-import 'package:http/http.dart' as http;
 
 class ApiPackagesRemoteDataSource implements PackagesRemoteDataSource {
-  ApiPackagesRemoteDataSource({http.Client? client})
-    : _client = client ?? http.Client();
+  ApiPackagesRemoteDataSource({Dio? dio}) : _dio = dio ?? Dio();
 
-  final http.Client _client;
+  final Dio _dio;
 
   @override
   Future<List<Package>> fetchPackages() async {
     final uri = PubApi.endpoint('packages');
-    final response = await _client.get(uri);
-    if (response.statusCode != 200) {
-      throw StateError('パッケージ取得に失敗しました。: ${response.statusCode}');
-    }
-
-    final json = jsonDecode(response.body) as Map<String, Object?>;
-    final dto = PackagesResponseDto.fromJson(json);
+    final response = await _dio.getUri<Map<String, Object?>>(uri);
+    final dto = PackagesResponseDto.fromJson(response.data!);
     return dto.packages.map((package) => package.toDomain()).toList();
   }
 }
